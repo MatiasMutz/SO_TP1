a410df9504e1086206caeb1400053eec15172759
lgonzalezrouco <lgonzalezrouco@itba.edu.ar>
2023-09-07 19:26:00 -0300

    🙋🏼‍♂️ using execlp instead of execve

--- slave.c
+++ slave.c
@@ -1,19 +1,18 @@
 #include "include/slave.h"
 
 int main(int argc, char *argv[]) {
-    printf("\nSoy el hijo %d\n", getpid());
+    
+/*     printf("\nSoy el hijo %d\n", getpid());
     char* buffer = malloc(256);
-    read(0, buffer, 256);
+    read(STDIN_FILENO, buffer, 256);
     printf("%s\n", buffer);
     free(buffer);
-    return 0;
+    return 0; */
 
-    /*
-    char *argv_md5sum[] = {"md5sum", "app.c", "slave.c", NULL};
+    char *arguments[] = {"md5sum", "app.c", "slave.c", NULL};
+    
+    execlp("md5sum", arguments, (char *) NULL);
 
-    execve("/usr/bin/md5sum", argv_md5sum, NULL);
-
-    perror("execve"); // execve() returns only on error
+    perror("execlp"); // execlp() returns only on error
     exit(EXIT_FAILURE);
-     */
 }
