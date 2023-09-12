49bd224764184e445dd790aed87f8d482bfc346b
MatiasMutz <mmutz@itba.edu.ar>
2023-09-12 20:54:26 -0300

    🎾some improvements
    🏊 here your emoji @bendeyhe
    
    Co-Authored-By: Lucas Gonzalez Rouco <108434505+lgonzalezrouco@users.noreply.github.com>

--- app.c
+++ app.c
@@ -1,9 +1,4 @@
 #include "include/app.h"
-#include <math.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
 
 typedef struct {
     int number;
@@ -12,6 +7,7 @@ typedef struct {
 } slave;
 
 //void sendToSlave(int slaveNumber, int fdsWrite[][2], char *path, int *filesRemaining, int *count) {
+/*
 void sendToSlave(slave slave[], int fdsWrite[][2], char *path, int *filesRemaining, int *count, int slaveNumber) {
     int length = strlen(path);
     //todo estaba bien declarar array no estático con tamaño variable o hay que usar malloc y free?
@@ -22,12 +18,37 @@ void sendToSlave(slave slave[], int fdsWrite[][2], char *path, int *filesRemaini
     (*filesRemaining)--;
     (*count)++;
     slave[slaveNumber].filesProcessed++;
+}*/
+
+
+void checkRealloc(const char *s) {
+    if (s == NULL) {
+        perror("Error in realloc");
+        exit(EXIT_FAILURE);
+    }
 }
 
+void sendToSlave(slave slave[], int fdsWrite[][2], const char *path, int *filesRemaining, int *count, int slaveNumber) {
+    char *toSend = NULL;
+    size_t len;
+    for(len = 0; path[len]; len++) {
+        if (len % BLOQUE == 0) {
+            toSend = realloc(toSend, sizeof(char) * (len + BLOQUE));
+            checkRealloc(toSend);
+        }
+        toSend[len] = path[len];
+    }
+    toSend = realloc(toSend, len + 1);
+    checkRealloc(toSend);
+    toSend[len] = ' ';
+    write(fdsWrite[slaveNumber][STDOUT_FILENO], toSend, len + 1);
+    free(toSend);
+    (*filesRemaining)--;
+    (*count)++;
+    slave[slaveNumber].filesProcessed++;
+}
 
 int main(int argc, char *argv[]) {
-    char *newargv[] = {NULL};
-    char *newenv[] = {NULL};
     int filesQty = argc - 1;
     int filesRemaining = filesQty;
 
@@ -46,14 +67,12 @@ int main(int argc, char *argv[]) {
 
     int count = 1;
 
-
     printf("Cantidad de archivos: %d\nCantidad de esclavos: %d\n", filesQty, slavesQty);
 
     for (int i = 0; i < slavesQty; i++) {
         slaves[i].number = i;
         slaves[i].filesProcessed = 0;
         if (pipe(fdsWrite[i]) < 0 || pipe(fdsRead[i]) < 0) {
-            // Creo un pipe, antes del fork asi ambos procesos lo tienen
             perror("pipe creation error");
             exit(EXIT_FAILURE);
         }
@@ -61,34 +80,33 @@ int main(int argc, char *argv[]) {
             // PADRE
             slaves[i].pid = pid;
             for (int j = 0; j < INITIAL_LOAD && count <= filesQty; j++) {
-               // sendToSlave(i, fdsWrite, argv[count], &filesRemaining, &count);
-               sendToSlave(slaves, fdsWrite, argv[count], &filesRemaining, &count, i);
+                // sendToSlave(i, fdsWrite, argv[count], &filesRemaining, &count);
+                sendToSlave(slaves, fdsWrite, argv[count], &filesRemaining, &count, i);
             }
-            close(fdsWrite[i][0]);
-
-            close(fdsRead[i][1]);
+            close(fdsWrite[i][STDIN_FILENO]);
+            close(fdsRead[i][STDOUT_FILENO]);
         } else {
             // HIJO
-            close(STDIN_FILENO); // Cerramos STDIN en padre
-            dup(fdsWrite[i][STDIN_FILENO]); // Dupeamos read end of pipe -> Lo manda a STDIN
-            close(fdsWrite[i][STDIN_FILENO]); // Cerrramos ambos extremos del pipe
+            close(STDIN_FILENO);
+            dup(fdsWrite[i][STDIN_FILENO]);
+            close(fdsWrite[i][STDIN_FILENO]);
             close(fdsWrite[i][STDOUT_FILENO]);
 
-            close(STDOUT_FILENO); // Cerramos STDOUT en padre
-            dup(fdsRead[i][STDOUT_FILENO]); // Dupeamos write end of pipe -> Lo manda a STDOUT
-            close(fdsRead[i][STDIN_FILENO]); // Cerrramos ambos extremos del pipe
+            close(STDOUT_FILENO);
+            dup(fdsRead[i][STDOUT_FILENO]);
+            close(fdsRead[i][STDIN_FILENO]);
             close(fdsRead[i][STDOUT_FILENO]);
-            execve("slave", newargv, newenv);
-            perror("Error in execve.");
+            execve("slave", (char *[]) {NULL}, (char *[]) {NULL});
+            perror("Error in execve");
             exit(EXIT_FAILURE);
         }
     }
 
+    char buffer[256] = {'\0'};
+    int charsRead;
     int retval;
     fd_set rdfs;
     int maxFd;
-    char buffer[256] = {'\0'};
-    int charsRead;
 
     int fd = open("output.txt", O_WRONLY | O_APPEND | O_CREAT, 0644);
 
@@ -103,28 +121,27 @@ int main(int argc, char *argv[]) {
         }
         retval = select(maxFd + 1, &rdfs, NULL, NULL, NULL); // espera a que haya info disponible en stdin
         if (retval == -1) {
-            perror("select()");
+            perror("Error in select");
             exit(EXIT_FAILURE);
         } else if (retval) {
-            for (int i = 0; i < slavesQty; i++) {
+            for (int i = 0; i < slavesQty && filesRemaining > 0; i++) {
                 if (FD_ISSET(fdsRead[i][STDIN_FILENO], &rdfs)) {
                     // si elimino un fd del set tengo que volver a calcular el maximo
                     charsRead = read(fdsRead[i][STDIN_FILENO], buffer, 256);
+
                     buffer[charsRead] = '\0';
                     dprintf(fd, "PID: %d %s", slaves[i].pid, buffer);
 
-                    if (filesRemaining > 0 && slaves[i].filesProcessed >= 5) {//aca hay que chequear que el esclavo ya haya terminado de procesar los primeros 5 archivos.
+                    if (slaves[i].filesProcessed >= INITIAL_LOAD) {//aca hay que chequear que el esclavo ya haya terminado de procesar los primeros 5 archivos.
                         sendToSlave(slaves, fdsWrite, argv[count], &filesRemaining, &count, i);
                     }
                 }
             }
         }
     }
-
     for (int i = 0; i < slavesQty; i++) {
         close(fdsRead[i][0]);
         close(fdsWrite[i][1]);
-
     }
     close(fd);
     return 0;
--- include/app.h
+++ include/app.h
@@ -3,6 +3,7 @@
 
 #define FILES_PER_SLAVE 20
 #define INITIAL_LOAD 5
+#define BLOQUE 5
 
 #include <math.h>
 #include <stdio.h>
@@ -11,5 +12,9 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <math.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #endif
