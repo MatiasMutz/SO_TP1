f3723c0403499296300b953ddc569c0e9d78688c
lgonzalezrouco <lgonzalezrouco@itba.edu.ar>
2023-09-12 21:51:49 -0300

    modified includes

--- include/app.h
+++ include/app.h
@@ -5,16 +5,15 @@
 #define INITIAL_LOAD 5
 #define BLOQUE 5
 
+#include <fcntl.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
+#include <sys/select.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <string.h>
-#include <math.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <unistd.h>
 
 #endif
--- include/slave.h
+++ include/slave.h
@@ -3,8 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 #endif
